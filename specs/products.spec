978750d79a309338aa20381a02cf41fc380c32eb
prateekbaheti <prateektheone@gmail.com>
2015-02-19 11:50:19 +0530

    Adding chrome execution via env chrome

--- specs/products.spec
+++ specs/products.spec
@@ -5,11 +5,12 @@ Create a new product
 --------------------
 
 * Create a product 
-     |Title         |Description         |Author       |Price|
-     |--------------|--------------------|-------------|-----|
-     |Go Programming|ISBN: 978-1453636671|John P. Baugh|25.00|
-     |The Way to Go |ISBN: 978-1469769165|Ivo Balbaert |20.00|
-     |Learning Go   |ebook               |Miek Gieben  |0.00 |
+     |Title         |Description         |Author        |Price|
+     |--------------|--------------------|--------------|-----|
+     |Go Programming|ISBN: 978-1453636671|John P. Baugh |25.00|
+     |The Way to Go |ISBN: 978-1469769165|Ivo Balbaert  |20.00|
+     |Go In Action  |ISBN: 9781617291784 |Brian Ketelsen|30.00|
+     |Learning Go   |ebook               |Miek Gieben   |0.00 |
 
 "Find and Open product page for" is a concept
 
--- src/test/java/com/thoughtworks/gauge/example/DriverFactory.java
+++ src/test/java/com/thoughtworks/gauge/example/DriverFactory.java
@@ -2,18 +2,27 @@ package com.thoughtworks.gauge.example;
 
 import com.thoughtworks.gauge.AfterSuite;
 import com.thoughtworks.gauge.BeforeSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-    public static FirefoxDriver getDriver() {
+    private static final String CHROME = "chrome";
+
+    public static WebDriver getDriver() {
         return driver;
     }
 
-    private static FirefoxDriver driver;
+    private static WebDriver driver;
 
     @BeforeSuite
     public void Setup() {
-        driver = new FirefoxDriver();
+        String browser = System.getenv("browser.name");
+        if (browser.toLowerCase().equals(CHROME)) {
+            driver = new ChromeDriver();
+        } else {
+            driver = new FirefoxDriver();
+        }
     }
 
     @AfterSuite
