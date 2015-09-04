fd41fea7474595b4c013cd5e34a37454f70bbf3d
Robert Kotlowski <rkotlows@thoughtworks.com>
2015-09-04 10:10:05 +0200

    rk|added edit product page definition
    providing webelements, updateProductValue and saveProduct function

---
+++ src/test/java/com/thoughtworks/gauge/example/pages/EditProductPage.java
@@ -0,0 +1,60 @@
+package com.thoughtworks.gauge.example.pages;
+
+import org.openqa.selenium.WebElement;
+
+public class EditProductPage extends ProductPage {
+
+    public static String EditProductUrl(String productId) {
+        return Url.concat("admin/products/"+productId+"/edit");
+    }
+
+    public WebElement product_title;
+
+    public WebElement product_description;
+
+    public WebElement product_author;
+
+    public WebElement product_price;
+
+    public WebElement product_featured;
+
+    public WebElement product_available_on_1i;
+
+    public WebElement product_available_on_2i;
+
+    public WebElement product_available_on_3i;
+
+    public WebElement product_image_file_name;
+
+    public WebElement product_submit;
+
+    public void saveProduct() {
+        product_submit.click();
+    }
+
+    public void updateProductValue(String specifier, String newValue) {
+        switch(specifier.toLowerCase()) {
+            case "title":
+                product_title.clear();
+                product_title.sendKeys(newValue);
+                break;
+            case "description":
+                product_description.clear();
+                product_description.sendKeys(newValue);
+                break;
+            case "author":
+                product_author.clear();
+                product_author.sendKeys(newValue);
+                break;
+            case "price":
+                product_price.clear();
+                product_price.sendKeys(newValue);
+                break;
+            case "imageFileName":
+                product_image_file_name.clear();
+                product_image_file_name.sendKeys(newValue);
+                break;
+        }
+        saveProduct();
+    }
+}
